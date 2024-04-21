04ed19bf7ede0a1f4798ceb081b21272d04f83c7
Peter Lu <peter.lu723@gmail.com>
2024-04-20 22:38:09 -0700

    created nav button hover animation

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,22 @@
-import { Gradient } from "whatamesh";
-const gradient = new Gradient();
-gradient.initGradient("#gradient-canvas");
+// import { Gradient } from "whatamesh";
+// const gradient = new Gradient();
+// gradient.initGradient("#gradient-canvas");
+
+
+const links = document.querySelectorAll(".nav-hover");
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    link.style.color = "white";
+    link.style.transform = "scale(1.1)";
+    link.style.transition = "transform 0.3s ease, color 0.3s ease";
+  });
+
+  link.addEventListener("mouseout", () => {
+    link.style.color = "";
+    link.style.transform = "scale(1)";
+    link.style.transition = "transform 0.3s ease, color 0.3s ease";
+  });
+});
+
+// alert("testing script.js")
